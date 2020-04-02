50e566b1036cd2a162334cf9e2745773248ad942
Juho Toivonen <juho.toivonen@myy.haaga-helia.fi>
2020-04-02 17:24:17 +0300

    First commit: Task 1 functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Customerlist from './components/Customerlist';
+import Trainingslist from './components/Trainingslist';
+import AppBar from './components/AppBar';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { Button } from '@material-ui/core';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar position="static">
+      </AppBar>
+      <Router>
+      <div>
+        <nav>
+          <ul id="menu">
+            <li>
+              <Link to="/"><Button variant="contained" color="primary">Customer list</Button></Link>
+            </li>
+            <li>
+              <Link to="/trainingslist"><Button variant="contained" color="primary">Trainings list</Button></Link>
+            </li>
+            <li>
+            <Link to="/calendar"><Button variant="contained" color="primary">Calendar</Button></Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/trainingslist">
+            <Trainingslist></Trainingslist>
+          </Route>
+          <Route path="/calendar">
+            <Calendar />
+          </Route>
+          <Route path="/">
+            <Customerlist></Customerlist>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
     </div>
   );
+}
+
+function Calendar() {
+  return <h2>Calendar</h2>;
 }
 
 export default App;
---
+++ src/components/Customerlist.js
@@ -0,0 +1,94 @@
+import React, { useState, useEffect, forwardRef } from 'react';
+import MaterialTable from 'material-table';
+
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
+import Delete from '@material-ui/icons/Delete';
+import FitnessCenter from '@material-ui/icons/FitnessCenter';
+
+export default function Customerlist() {
+    const tableIcons = {
+        Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+        Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+        Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+        Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+        DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+        Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+        Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+        Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+        FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+        LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+        NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+        PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+        ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+        Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+        SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+        ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+        ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+    };    
+
+    const [customers, setCustomers] = useState([]);
+
+    const fetchData = () => {
+        fetch('https://customerrest.herokuapp.com/api/customers')
+        .then(response => response.json())
+        .then(data => setCustomers(data.content))
+    }
+
+    useEffect(() => fetchData(), []);
+
+    console.log(customers)
+
+    return (
+        <MaterialTable
+            icons={tableIcons}
+            options={{
+                pageSize: 10,
+                pageSizeOptions: [5, 10, 20, 50],
+                toolbar: true,
+                paging: true
+            }}
+            title="Customer list"
+            columns={[
+                { title: 'First name', field: 'firstname' },
+                { title: 'Last name', field: 'lastname' },
+                { title: 'Email', field: 'email' },
+                { title: 'Phone', field: 'phone' },
+                { title: 'Address', field: 'streetaddress'},
+                { title: 'Postcode', field: 'postcode'},
+                { title: 'City', field: 'city'}
+            ]}
+            data={customers}
+            actions={[
+                {
+                    icon: Edit,
+                    tooltip: 'Edit User',
+                    onClick: (event, rowData) => alert("You edited " + rowData.name)
+                },
+                {
+                    icon: Delete,
+                    tooltip: 'Delete User',
+                    onClick: (event, rowData) => window.confirm("You want to delete " + rowData.name)
+                },
+                {
+                    icon: FitnessCenter,
+                    tooltip: 'Add Training',
+                    onClick: (event, rowData) => window.confirm("You want to delete " + rowData.name)
+                }
+              ]} 
+        />
+    )
+}
